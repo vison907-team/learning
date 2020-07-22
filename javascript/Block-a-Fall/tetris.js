a8e70dac0ed29939d9c9977d818c7d53f9ea3b93
Vyom Srivastava <vyomsrivastava@veridenkey324.connect>
2020-07-22 13:33:43 +0100

    Fixed tetromino bug and added function

--- javascript/Block-a-Fall/tetris.js
+++ javascript/Block-a-Fall/tetris.js
@@ -88,4 +88,19 @@ document.addEventListener('DOMContentLoaded', () => {
             draw()
         }
     }
+
+    // move the tetromino left unless it is at the edge or there is a blockage
+
+    function moveLeft() {
+        undraw()
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+
+        if(!isAtLeftEdge) currentPosition -=1
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+        currentPosition +=1
+        }
+
+        draw()
+    }
 })

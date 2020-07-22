a31f0421c7f5a3f2a481e0fed88fe4f5ee0114e6
Vyom Srivastava <vyomsrivastava@veridenkey324.connect>
2020-07-22 13:59:34 +0100

    added move left function for basic gameplay

--- javascript/Block-a-Fall/tetris.js
+++ javascript/Block-a-Fall/tetris.js
@@ -69,6 +69,20 @@ document.addEventListener('DOMContentLoaded', () => {
     //make the tetromino move down every second
     timerID = setInterval(moveDown, 1000)
 
+    //assign functions to keyCodes
+    function control(e) {
+        if(e.keyCode === 37) {
+        moveLeft()
+        } else if(e.keyCode === 38) {
+            //rotate()
+        } else if(e.keyCode === 39) {
+            //moveRight
+        } else if(e.keyCode == 40) {
+            moveDown()
+        }
+    }
+    document.addEventListener('keyup', control)
+
     //move down function
     function moveDown() {
         undraw()
@@ -90,7 +104,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // move the tetromino left unless it is at the edge or there is a blockage
-
     function moveLeft() {
         undraw()
         const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)

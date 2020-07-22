4df27b3ec77b27cbf7800b802608f99006ed97de
Vyom Srivastava <vyomsrivastava@veridenkey324.connect>
2020-07-22 17:21:20 +0100

    added rotate function

--- javascript/Block-a-Fall/tetris.js
+++ javascript/Block-a-Fall/tetris.js
@@ -74,7 +74,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if(e.keyCode === 37) {
         moveLeft()
         } else if(e.keyCode === 38) {
-            //rotate()
+            rotate()
         } else if(e.keyCode === 39) {
             moveRight()
         } else if(e.keyCode == 40) {
@@ -128,6 +128,17 @@ document.addEventListener('DOMContentLoaded', () => {
         currentPosition -=1
         }
 
+        draw()
+    }
+
+    //rotate the tetromino
+    function rotate() {
+        undraw()
+        currentRotation ++
+        if(currentRotation === current.length) { //if the current rotation gets to 4, make it go back to 0
+            currentRotation = 0
+        }
+        current = theTetrominoes[random][currentRotation]
         draw()
     }
 })

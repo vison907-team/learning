e57bbba5ec627e88ae67f9ec41135672f5563218
Vyom Srivastava <vyomsrivastava@veridenkey324.connect>
2020-07-22 09:12:36 +0100

    Fixed freeze bug

--- javascript/Block-a-Fall/tetris.js
+++ javascript/Block-a-Fall/tetris.js
@@ -77,16 +77,15 @@ document.addEventListener('DOMContentLoaded', () => {
         freeze()
     }
 
-    //freeze function
-  function freeze() {
-    if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
-      current.forEach(index => squares[currentPosition + index].classList.add('taken'))
-      //start a new tetromino falling
-      random = Math.floor(Math.random() * theTetrominoes.length)
-      current = theTetrominoes[random][currentRotation]
-      currentPosition = 4
-      draw()
-       }
+  //freeze function
+    function freeze() {
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+            //start a new tetromino falling
+            random = Math.floor(Math.random() * theTetrominoes.length)
+            current = theTetrominoes[random][currentRotation]
+            currentPosition = 4
+            draw()
+        }
     }
-    
 })
